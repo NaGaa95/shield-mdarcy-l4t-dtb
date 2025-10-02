a339d8d10d277651ffd48320825e3859fe01ab08
NaGa <38852591+NaGaa95@users.noreply.github.com>
2025-10-02 04:04:04 +0200

    build script

---
+++ dtimg.sh
@@ -0,0 +1,4 @@
+mkdtimg create shield-tv-2019.dtimg --page_size=0x1000 \
+	0-2894-40-A8.dtb    --id=0xb4e --rev=0xa8  --custom0=0x28  \
+	1-2894-2551-B00.dtb --id=0xb4e --rev=0xb00 --custom0=0x9f7 \
+	2-2894-3551-B03.dtb --id=0xb4e --rev=0xb03 --custom0=0xddf
